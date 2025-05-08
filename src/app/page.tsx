449bed6d918fb1afb947e735f7c1466cec6013bf
JAY <JAY@oonne.com>
2025-05-08 16:21:07 +0800

    feat: 静态资源

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
-          className="dark:invert"
           src="/img/logo.png"
           alt="logo"
           width={180}
@@ -13,7 +12,7 @@ export default function Home() {
           priority
         />
         <div>
-          <h1>测试分支</h1>
+          <h1>测试首页</h1>
         </div>
       </main>
     </div>
