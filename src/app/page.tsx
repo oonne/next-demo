069c96987cf5e8ca29f2d500e6fdfb3e40b772c1
JAY <JAY@oonne.com>
2025-05-09 11:36:10 +0800

    refactor: 格式化

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          src="/img/logo.png"
-          alt="logo"
-          width={180}
-          height={38}
-          priority
-        />
+        <Image src="/img/logo.png" alt="logo" width={180} height={38} priority />
         <div>
           <h1>首页</h1>
           <Link href="/page-1">跳转到page-1</Link>
