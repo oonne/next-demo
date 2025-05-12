b9b300d16a7996977304fd756ff75927a51ea005
JAY <JAY@oonne.com>
2025-05-12 11:30:20 +0800

    feat: 测试部署

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Next-demo
+
 Next-demo
+
+# 功能
+
+- prettier
+- 部署到cloudflare
+
+- 自定义组件
+- 常用utils
+- 环境变量
+- 国际化
+- SEO
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   distDir: 'out',
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  assetPrefix: '/',
+  basePath: '',
 };
 
 export default nextConfig;
