700027f45f1f2fce7a3329eb93b555eee094a56a
JAY <JAY@oonne.com>
2025-05-12 17:14:12 +0800

    feat: 部署配置

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // 静态资源路径
-  assetPrefix: '/',
-  // 基础路径
-  basePath: '',
-
-  // 图片优化
-  images: {
-    unoptimized: true,
-  },
+  // // 静态资源路径
+  // assetPrefix: '/',
+  // // 基础路径
+  // basePath: '',
+  // // 图片优化
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
