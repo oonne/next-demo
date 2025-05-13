e97ddb80441a64d5f82aa84e617ac9f55a876b08
JAY <JAY@oonne.com>
2025-05-13 17:07:41 +0800

    feat: 引入新文件

---
+++ src/i18n/modules/basic.ts
@@ -0,0 +1,21 @@
+/*
+ * 国际化翻译
+ */
+const msg: ILangMsgs = {
+  lang_name: {
+    en_US: 'English',
+    zh_CN: '中文（简体）',
+    zh_TW: '中文（繁体）',
+    es_ES: 'Español',
+    fr_FR: 'Français',
+    ru_RU: 'Русский',
+    pt_PT: 'Português',
+    de_DE: 'Deutsch',
+    ja_JP: '日本語',
+    it_IT: 'Italiano',
+    ko_KR: '한국어',
+    vi_VN: 'Tiếng Việt',
+  },
+};
+
+export default msg;
