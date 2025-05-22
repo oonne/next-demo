1131797eab7134240e683da15cf15140f9a5a87d
JAY <JAY@oonne.com>
2025-05-22 15:36:51 +0800

    feat: 多语言重写

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,25 +11,39 @@ export function middleware(request: NextRequest) {
   );
 
   if (pathnameHasLocale) {
+    // 从路径中提取语言代码
+    const locale = pathname.split('/')[1];
+
     // 如果访问的是 /${DEFAULT_LANGUAGE}，重定向到首页
     if (pathname === `/${DEFAULT_LANGUAGE}`) {
       const url = request.nextUrl.clone();
       url.pathname = '/';
       return NextResponse.redirect(url);
     }
-    return;
+
+    // response 设置 X-NEXT-INTL-LOCALE 头部 为 locale
+    const response = NextResponse.next();
+    response.headers.set('X-NEXT-INTL-LOCALE', locale);
+    return response;
   }
 
   // 如果路径没有包含语言前缀，则默认为英文
   const url = request.nextUrl.clone();
+
   // 首页保留原路径
   if (pathname === '/') {
     url.pathname = `/${DEFAULT_LANGUAGE}`;
-    return NextResponse.rewrite(url);
+    const response = NextResponse.rewrite(url);
+    response.headers.set('X-NEXT-INTL-LOCALE', DEFAULT_LANGUAGE);
+    return response;
   }
+
   // 子页面重定向
   url.pathname = `/${DEFAULT_LANGUAGE}${pathname}`;
-  return NextResponse.redirect(url);
+  const response = NextResponse.redirect(url);
+  // 关键部分：手动设置 X-NEXT-INTL-LOCALE 头部
+  response.headers.set('X-NEXT-INTL-LOCALE', DEFAULT_LANGUAGE);
+  return response;
 }
 
 export const config = {
